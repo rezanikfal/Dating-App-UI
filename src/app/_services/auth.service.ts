e8156448e85764d2abfde07ae5c228b00622c939
Reza Nikfal <mr.nikfal@gmail.com>
2020-07-06 21:50:59 -0500

    Wire Login/Logout

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -12,12 +12,12 @@ export class AuthService {
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model).pipe(
-      map((response:any)=>{
-        const user=response
-        if(user){
-          localStorage.setItem('token',user.token)
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
         }
       })
-    )
+    );
   }
 }
